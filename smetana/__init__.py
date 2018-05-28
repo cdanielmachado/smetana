a416e18dd24cc2ba1352f0fd0833eccba8893065
Daniel <cdanielmachado@gmail.com>
2018-05-28 15:02:26 +0200

    fix import statements

--- smetana/__init__.py
+++ smetana/__init__.py
@@ -2,6 +2,8 @@
 
 """Top-level package for smetana."""
 
-__author__ = """Daniel Machado"""
+__author__ = "Daniel Machado"
 __email__ = 'cdanielmachado@gmail.com'
 __version__ = '0.1.0'
+
+from .smetana import mip_score, mro_score, species_coupling_score, metabolite_production_score, metabolite_uptake_score
